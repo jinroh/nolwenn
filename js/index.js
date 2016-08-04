1de08ecf9e62d6636f9670e8f19fbcedda307d2e
Loïc Le Guen <loic.leguen@ensci.com>
2016-08-04 18:12:34 +0200

    mofifs

--- js/index.js
+++ js/index.js
@@ -50,7 +50,7 @@ const SiteHeader = React.createClass({
       <header className="site-header">
         <h1>Nolwenn  Le Scao</h1>
         <p>
-          <Link to="/about">À propos</Link> | <Link to="/contact">Contact</Link>
+          <a href="../public/pdf/Curiculum_Vitae-Nolwenn_Le_Scao.pdf" download="Curiculum_Vitae-Nolwenn_Le_Scao">À propos</a> | <Link to="/contact">Contact</Link>
         </p>
       </header>
     );
